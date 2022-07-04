88b23a5b1f9fbb9373dae41783102ee5314abe90
00246506 <alberto.bregliano@telecomitalia.it>
2022-07-04 13:39:42 +0200

    rem

--- src/components/SendDocuments.js
+++ src/components/SendDocuments.js
@@ -12,11 +12,11 @@ export class SendDocuments extends Component {
         <Card>
             {/* <Card.Img variant="top" src={UploadFile} /> */}
             <Card.Body>
-              {/* <Card.Title>Upload documents</Card.Title> */}
+              {/* <Card.Title><small>Get more info about the company</small></Card.Title> */}
               <Card.Text>
-              <a href={report} attributes-list download><Button >Get more info of the company</Button></a>
-              <span>   </span>
-              <a href='https://www.dropbox.com/request/3anRrp91CfqQfcsmD2fB' target="_blank" attributes-list download><Button>Upload your documents</Button></a>
+              <a href={report} attributes-list download><Button className='m-1'>Company Info</Button></a>
+              
+              <a href='https://www.dropbox.com/request/3anRrp91CfqQfcsmD2fB' target="_blank" attributes-list download className='m-1'><Button >Upload your documents</Button></a>
               </Card.Text>
             </Card.Body>
         </Card>
